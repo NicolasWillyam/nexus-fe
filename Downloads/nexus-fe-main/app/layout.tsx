c6b4a426eb00bb4ee0226cdace6aa83a47070ec3
linh-ha18 <hathljnk@gmail.com>
2026-09-16 20:58:44 +0700

    feature: dashboard

---
+++ Downloads/nexus-fe-main/app/layout.tsx
@@ -0,0 +1,31 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+// Khởi tạo font Inter
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="vi" className={inter.className} suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
